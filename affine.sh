a92126094ba9029296d618bd7b1632014daa5b3a
Catherine(Ziyi) Liu <cliu2315@gmail.com>
2018-04-29 15:35:11 -0400

    Update affine.sh

--- affine.sh
+++ affine.sh
@@ -1,2 +1,2 @@
-# parameters: sequence1, sequence2, scoringMatrix, gapPenalty
-python affineGlobal.py $1 $2 $3 $4
+# parameters: sequence1, sequence2, scoringMatrix, gapPenalty, affinePenalty
+python affineGlobal.py $1 $2 $3 $4 $5
